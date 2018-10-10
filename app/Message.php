f74c985daa195a76539b8ced46b9395228055e3f
maximeaudy <maxime.audy@epsi.fr>
2018-10-10 23:55:58 +0200

    Gérer les return des formulaires PHP avec une classe PHP Message.php et Message.js pour gérer le JS ou renvoie du message dans une div

---
+++ app/Message.php
@@ -0,0 +1,41 @@
+<?php
+namespace lambda;
+
+/**
+ * Class Message
+ * @package lambda
+ */
+class Message
+{
+    /**
+     * @var string $type État du message (success, error...)
+     */
+    private $type;
+
+    /**
+     * @var string Contenu du message
+     */
+    private $message;
+
+    /**
+     * @var int Méthode a utiliser (div, js...)
+     */
+    private $method;
+
+    public function __construct($type, $message, $method)
+    {
+        $this->type = $type;
+        $this->message = $message;
+        $this->method = $method;
+
+        $this->getMessage();
+    }
+
+    private function getMessage()
+    {
+        if($type === 1)
+            echo json_encode(array($this->type, $this->message, $this->method));
+        else
+            echo $this->message;
+    }
+}
