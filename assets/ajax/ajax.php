88cd95643e45da86125b0396dacd43135887766c
maximeaudy <maxime.audy@epsi.fr>
2018-10-15 17:17:47 +0200

    MAJ par rapport a test.php

--- assets/ajax/ajax.php
+++ assets/ajax/ajax.php
@@ -1,21 +1,25 @@
 <?php
-$class = $_GET['class'];
-$method = $_GET['method'];
-
-use lambda\Users;
-
 require('../../app/Autoload.php');
 
-if(isset($class) AND isset($method)){
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    $class = $_GET['class'];
+    $method = $_GET['method'];
+    $data = $_POST;
 
-	//Vérification s'il y a une sécurisation d'éditeur de texte
-    if(isset($data['editor'])){
-        $validation = true;
-    }else{
-        $validation = false;
+    if(isset($class) AND isset($method)){
+
+        //Vérification s'il y a une sécurisation d'éditeur de texte
+        if(isset($data['editor'])){
+            $validation = true;
+        }else{
+            $validation = false;
+        }
+
+        $data = Secure_array($data, $validation);
+
+        $init = new $class(0);
+        $init->$method($data);
     }
-
-    $data = Secure_array($_POST, $validation);
-	$te = new Users(0);
-	$te->$method($data);
+}else{
+    echo "Impossible d'accéder à ce contenu";
 }
