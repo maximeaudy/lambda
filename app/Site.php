e529596fd07d2802918731136f4c7fa933fdfeab
maximeaudy <maxime.audy@epsi.fr>
2018-10-17 19:15:49 +0200

    Mise au propre

--- app/Site.php
+++ app/Site.php
@@ -1,15 +1,24 @@
 <?php
 namespace lambda;
 
+/*
+ * Gère les paramètres du site
+ */
 class Site{
 	public $siteName;
 	public $siteLink;
 
+    /**
+     * Données de connexion à la base de donnée
+     */
 	protected static $sqlHost;
 	protected static $sqlName;
 	protected static $sqlUser;
 	protected static $sqlPassword;
 
+    /**
+     * Initialisation du site avec ses paramètres
+     */
 	public function __construct($data){
 		//Paramètres du site
 		$this->siteName = $data['siteName'];
@@ -22,6 +31,10 @@ class Site{
 		self::$sqlPassword = $data['sqlPassword'];
 	}
 
+    /**
+     * Retourne la page courante
+     * @return string Page courante suivis de l'extension
+     */
 	public function getCurrentPage(){
 		$currentPage = array_reverse(explode("/", $_SERVER['REQUEST_URI']));
 		return $currentPage[0];
